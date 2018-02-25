f8297e9ea88fb44482d7d9908757c26b2326a165
Liam <liamschmidt14@gmail.com>
2018-02-25 16:50:01 -0500

    Created a new struct Vertex designed to hold path and visitation information for the BFS.

--- main.h
+++ main.h
@@ -5,5 +5,13 @@
 #ifndef MINIMALNODESPATHS_MAIN_H
 #define MINIMALNODESPATHS_MAIN_H
 #include <vector>
+
 using namespace std;
+struct Vertex {
+    bool visitedFromStart = false;
+    bool visitedFromEnd = false;
+
+    vector<int> pathFromStart;
+    vector<int> pathFromEnd;
+};
 #endif //MINIMALNODESPATHS_MAIN_H
